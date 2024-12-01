4e0c45590389e9d98847b3516dba680b0e437cea
Mohit Chouhan <info.mohit321@gmail.com>
2024-12-01 13:02:13 +0000

    Add functions to display weather data fetched from API and to handle errors

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const cityNameDisplay = document.getElementById("city-name");
     const temperatureDisplay = document.getElementById("temperature");
     const descriptionDisplay = document.getElementById("description");
+    const feelsLikeDisplay = document.getElementById("feels-like");
+    const iconDisplay = document.getElementById("icon");
     const errorMessage = document.getElementById("error-message");
 
     const API_KEY = "ced7776227de7203a76aab3afcbd720c";
@@ -24,11 +26,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const fetchWeatherData = async (city) => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
         const response =  await fetch(url);
-        console.log("Response",response);
         if(!response.ok) {
             throw new Error("City not found");
         }
         const data = await response.json();
         return data;
     }
+
+    const displayWeatherData = (data) => {
+        const {name, main, weather} = data;
+        cityNameDisplay.textContent = name;
+        temperatureDisplay.textContent = `Temperature : ${main.temp} °C`;
+        descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
+        feelsLikeDisplay.textContent = `Feels like : ${main.feels_like} °C`;
+        iconDisplay.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather[0].icon}.png" alt="${weather[0].description}">`;
+        weatherInfo.classList.remove("hidden");
+        errorMessage.classList.add("hidden");
+    }
+
+    function showError() {
+        weatherInfo.classList.remove("hidden");
+        errorMessage.classList.add("hidden");
+      }
 });
